a9c6f0094a07ca32a68f9b22f1ca53c2edc7dabe
Brandon Yao <randomcallofduy@gmail.com>
2019-01-23 18:11:56 -0600

    Edit index

--- Frontend/test-next/pages/index.js
+++ Frontend/test-next/pages/index.js
@@ -1,7 +1,22 @@
-const Index = () => (
-    <div>
-        <p>Hello Next.js</p>
-    </div>
+import Layout from '../components/Layout.js'
+import Link from 'next/link'
+import Fetch from 'isomorphic-unfetch'
+
+const PostLink = (props) => (
+    <li>
+        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
 )
 
-export default Index
+export default () => (
+    <Layout>
+        <h1>My Site</h1>
+        <u1>
+            <PostLink id="Hello-Nextjs" title="Hello Next.js"/>
+            <PostLink id="Learn-Nextjs" title="Learn Next.js"/>
+            <PostLink id="Beep!" title="Beep!"/>
+        </u1>
+    </Layout>
+)
