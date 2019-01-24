0bed705ae8963f4d71ecfc0cf3f1111506ec1084
Brandon Yao <randomcallofduy@gmail.com>
2019-01-23 18:12:05 -0600

    Add post.js

---
+++ Frontend/test-next/pages/post.js
@@ -0,0 +1,23 @@
+import {withRouter} from 'next/router'
+import Layout from '../components/Layout'
+import Fetch from 'isomorphic-unfetch'
+
+const Post = (props) => (
+    <Layout>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+        <img src = {props.show.image.medium}/>
+    </Layout>
+)
+
+Post.getInitialProps = async function(context) {
+    const { id } = context.query
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+    const show = await res.json()
+
+    console.log(`Fetched show: ${show.name}`)
+    
+    return { show }
+}
+
+export default Post
