695527dfd46211649c904c55b01b4e66615c7f0e
Austin Lee <alee62@lsu.edu>
2019-02-10 15:55:03 -0600

    Added month and date for the meeting time
    Added month and date selection in the drop down menu.

--- Frontend/test-next/pages/MeetingSchedule.js
+++ Frontend/test-next/pages/MeetingSchedule.js
@@ -6,7 +6,6 @@ const MeetingSchedule = () => (
 	<Layout>
 		<div>
 			<h1>Schedule a time and place too meet.</h1>
-			//Uses the Choices class to display the drop box menu of the meeting places
 			<Choices/>		
 		</div>
 	</Layout>
