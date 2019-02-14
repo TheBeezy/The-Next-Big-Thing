edc5526a771fc572137d9232241656fcd58846e8
ssarai1 <sunnyssarai98@gmail.com>
2019-02-14 08:53:40 -0600

    Update userpage.js

--- Frontend/test-next/pages/userpage.js
+++ Frontend/test-next/pages/userpage.js
@@ -1,8 +1,25 @@
-import Layout from '../components/Layout'
+﻿import Layout from '../components/Layout'
 
 export default () => (
     //Insert Code Here
     <Layout>
-
+		<head>
+			<title>
+				User Page
+			</title>
+		</head>
+		<body>
+			<h1>Juancho Ramirez </h1>
+			<h6> valued user since Dec. 1st 2018</h6>
+			<p>Hola. Mi llamo Juancho. Vine a la universidad para aprender biología ya que siempre quise estudiar plancton. Lamentablemente, soy un pobre estudiante universitario. Ayúdenme enumerando libros baratos y le daré un precio justo.</p>
+			<a href="www.translate.google.com" target="blank_">
+				Si no comprendas.
+			</a>
+		</body>
+		<footer id="userFooter">
+			<p>Contact us!</p>
+			<p>fakebusiness@gmail.com</p>
+			<p>(314) 159 - 2653</p>
+		</footer>
     </Layout>
 )
