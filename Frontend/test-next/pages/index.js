7d98444a4fc545dd879498a143ebe90bbe823a41
Brandon Yao <TheBeezy@users.noreply.github.com>
2019-02-13 18:50:05 -0600

    Remove node_modules

--- Frontend/test-next/pages/index.js
+++ Frontend/test-next/pages/index.js
@@ -2,21 +2,12 @@ import Layout from '../components/Layout.js'
 import Link from 'next/link'
 import Fetch from 'isomorphic-unfetch'
 
-const PostLink = (props) => (
-    <li>
-        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
-)
-
 export default () => (
     <Layout>
-        <h1>My Site</h1>
-        <u1>
-            <PostLink id="Hello-Nextjs" title="Hello Next.js"/>
-            <PostLink id="Learn-Nextjs" title="Learn Next.js"/>
-            <PostLink id="Beep!" title="Beep!"/>
-        </u1>
+        <h1>BookMill</h1>
+        <p>
+            <input type="button"></input>
+            <input type="text" size="100"></input>
+        </p>
     </Layout>
 )
