837d80d290df5c4010a113c8dbb1e80e4a0b62d8
ssarai1 <sunnyssarai98@gmail.com>
2019-02-14 09:42:41 -0600

    Update userpage.js

--- Frontend/test-next/pages/userpage.js
+++ Frontend/test-next/pages/userpage.js
@@ -3,6 +3,19 @@ import Layout from '../components/Layout'
 export default () => (
     //Insert Code Here
     <Layout>
-
+		<head>
+			<title>
+				Profile Page
+			</title>
+		</head>
+		<body>
+			<h1>Juancho Ramirez<h1>
+			<h6> valued user since December 14, 2018<h6>
+			<p>Hey guys!</p>
+			<p>My name is Juancho. I'm a student in LSU and I would like to keep my meetings short and to the point.</p>
+			<p>I prefer to meet at the Union or Middleton and I'm available at all hours of the day.</p>
+			<p>I added my paypal option to  my profile. When payment comes through I will meet.</p>
+			<p>I will pursue chargebacks to the fullest extend of the law.</p>
+		</body>
     </Layout>
 )
