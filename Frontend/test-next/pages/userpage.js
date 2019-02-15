02cbaa19f6618e821ee4ca8c4e04c32a5427817a
Stephen Watson <sawblade25@gmail.com>
2019-02-15 15:04:56 -0600

    Update userpage.js

--- Frontend/test-next/pages/userpage.js
+++ Frontend/test-next/pages/userpage.js
@@ -16,6 +16,8 @@ export default () => (
 			<p>I prefer to meet at the Union or Middleton and I'm available at all hours of the day.</p>
 			<p>I added my paypal option to  my profile. When payment comes through I will meet.</p>
 			<p>I will pursue chargebacks to the fullest extend of the law.</p>
+            <img src = "static/paypalLogo.png" href="https://www.paypal.com/donate/?token=N6WU3_egS4rnre1IdA6HQuNnzq1crlliR5Rs2f_c-h0L-ITqj8OpLj1PM5of0ccm9wyZSG&country.x=US&locale.x=US">
 		</body>
+        
     </Layout>
 )
