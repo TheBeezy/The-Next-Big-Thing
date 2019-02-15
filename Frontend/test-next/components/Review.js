d203c996ed396ea7bb89d811eb25f8d460b12702
ssarai1 <sunnyssarai98@gmail.com>
2019-02-15 16:54:46 -0600

    Create Review.js

---
+++ Frontend/test-next/components/Review.js
@@ -0,0 +1,40 @@
+class Review extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(){
+        document.getElementById("result").innerHTML = 
+        "Name:&nbsp;"+document.getElementById('name').value + 
+        "<p/>Rating:&nbsp;" + document.getElementById('grade').value + 
+        "<p/>Comments:<p/>" + document.getElementById('coment').value;
+    }
+
+
+
+    render() {
+        return (
+            <div>
+            <form action="PayslipServlet" method="get">
+            <p>Name:<input type="text" name="name" id="name"></input></p>
+            <p>Ratin(A-F):<select onChange name="grade" id="grade">
+                <option value = "A">A</option>
+				<option value = "B">B</option>
+				<option value = "C">C</option>
+                <option value = "D">D</option>
+                <option value = "E">E</option>
+                <option value = "F">F</option>
+            </select>       
+            </p>
+            <p>Comment: <textarea name="coment" id ="coment"></textarea></p>
+            <p><input type="button" value="Submit" onClick={this.handleSubmit}></input></p>
+
+            <span id="result"></span>
+
+            </form>
+            </div>
+        )
+    }
+}
+
+export default Review;
