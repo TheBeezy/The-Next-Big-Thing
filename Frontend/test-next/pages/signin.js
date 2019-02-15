815cc9a1e13dfb4130e08127f0ebab9ffb19ad69
Stephen Watson <sawblade25@gmail.com>
2019-02-15 14:33:38 -0600

    Update signin.js

--- Frontend/test-next/pages/signin.js
+++ Frontend/test-next/pages/signin.js
@@ -2,5 +2,8 @@ import SignInScreen from '../components/SignInScreen'
 import Layout from '../components/Layout'
 
 export default () => (
+    <head>
+        <title>Login</title>
+    </head>
   <SignInScreen/>
 )
