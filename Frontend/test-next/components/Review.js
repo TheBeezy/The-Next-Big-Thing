c230ae10d3355c4933c9016433f0e1c09017870a
Mrwingit <46791344+Mrwingit@users.noreply.github.com>
2019-02-15 00:29:40 -0600

    Update Review.js
    A little bit tweak.

--- Frontend/test-next/components/Review.js
+++ Frontend/test-next/components/Review.js
@@ -12,18 +12,13 @@ class Review extends React.Component {
 
 
 
-
     render() {
         return (
             <div>
-            <h1>Please Review Us!</h1>
-            <p><a href="http://localhost:3000/index">Home</a></p>
-
             <form action="PayslipServlet" method="get">
             <p>Name:<input type="text" name="name" id="name"></input></p>
             <p>Rating(A-F):<input type="text" name="rating" id="rate"></input></p>
             <p>Comment: <textarea name="coment" id ="coment"></textarea></p>
-
             <p><input type="button" value="Submit" onClick={this.handleSubmit}></input></p>
 
             <span id="result"></span>
