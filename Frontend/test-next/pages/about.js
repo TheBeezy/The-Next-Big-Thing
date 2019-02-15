965abf36cca40c3a8c2db3c39c891bd9f4cc0a43
Stephen Watson <sawblade25@gmail.com>
2019-02-15 14:33:59 -0600

    Update about.js

--- Frontend/test-next/pages/about.js
+++ Frontend/test-next/pages/about.js
@@ -2,7 +2,10 @@ import Layout from '../components/Layout.js'
 
 export default () => (
     <Layout>
-        <p>About Page</p>
+        <head>
+            <title>About Us</title>
+        </head>
+        <p>About Us</p>
 	Select a meeting location:
 		<select id = "myList" onchange = "meetingLocation()">
 			<option>PFT</option>
