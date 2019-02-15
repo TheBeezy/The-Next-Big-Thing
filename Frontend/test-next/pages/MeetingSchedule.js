2a7b88b253371979a3c7701c4d98125f6db0505f
Stephen Watson <sawblade25@gmail.com>
2019-02-15 14:33:51 -0600

    Update MeetingSchedule.js

--- Frontend/test-next/pages/MeetingSchedule.js
+++ Frontend/test-next/pages/MeetingSchedule.js
@@ -4,6 +4,9 @@ import Choices from '../components/Choices'
 //Create a const for a cleaner look
 const MeetingSchedule = () => (
 	<Layout>
+        <head>
+            <title>Schedule</title>
+        </head>
 		<div>
 			<h1>Schedule a time and place too meet.</h1>
 			<Choices/>		
