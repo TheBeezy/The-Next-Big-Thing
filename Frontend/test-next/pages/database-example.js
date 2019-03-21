205e4c435e3f94fa24c0fd24cf0f26fece26e48a
Brandon Yao <TheBeezy@users.noreply.github.com>
2019-03-21 13:59:27 -0500

    Add database example page

---
+++ Frontend/test-next/pages/database-example.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout'
+import SignInScreen from '../components/SignInScreen'
+import Firebase from 'firebase'
+import Database from '../components/Database'
+
+export default () => (
+    <Layout>
+        <Database/>
+    </Layout>
+)
