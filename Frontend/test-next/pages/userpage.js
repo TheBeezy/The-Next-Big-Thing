cf23c97a99bd96d80bb43451d02d4227af08e37d
ssarai1 <sunnyssarai98@gmail.com>
2019-04-02 02:42:51 -0500

    Clean Userpage for Milestone 2

--- Frontend/test-next/pages/userpage.js
+++ Frontend/test-next/pages/userpage.js
@@ -17,19 +17,11 @@ export default () => (
 			<br/>
 			<br/>
 			<br/>
-            <div>
-				<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=XHPRF8RJZKBHS&lc=US&item_name=CSGO%20Nades&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted" target="_blank">
-					<img id="paypal" src = "static/paypalLogo.png" width="108" height="108"/>
-				</a>
-			</div>
-			<form action="/editUserpage">
-				<input type="submit" value="Edit Profile Page" />
-			</form>
 		</body>
 		<footer id="userfooter">
-			<p>Contact us!</p>
-			<p>fakebusiness@gmail.com</p>
-			<p>(314) 159 - 2653</p>
+			<div>
+				<a href="/about">Contact us!</a>
+			</div>
 		</footer>
 		</html>
     </Layout>
