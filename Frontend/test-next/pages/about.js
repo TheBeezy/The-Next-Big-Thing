1f893edca472cd297f1e2afe1bf2ef2592943bf1
Brandon Yao <TheBeezy@users.noreply.github.com>
2019-04-02 17:14:27 -0500

    Edit about page to include all info

--- Frontend/test-next/pages/about.js
+++ Frontend/test-next/pages/about.js
@@ -9,12 +9,21 @@ export default () => (
 		<h4>For all business inquiries, contact us at the following places:</h4>
 		<p>Shivkaran Sarai:</p>
 		<p>ssarai1@lsu.edu</p>
-		<p>(318)801-3772</p>
 		<br/>
 		<p>Stephen Watson</p>
 		<p>swats18@lsu.edu</p>
-		<p>(225)394-8138</p>
 		<br/>
-	<p>...</p>
+		<p>Brandon Yao</p>
+		<p>byao3@lsu.edu</p>
+		<br/>
+		<p>Austin Lee</p>
+		<p>alee62@lsu.edu</p>
+		<br/>
+		<p>Hieu Mai</p>
+		<p>hmai5@lsu.edu</p>
+		<br/>
+		<p>Thein Tran</p>
+		<p>Ttra174@lsu.edu</p>
+		<br/>
     </Layout>
 )
