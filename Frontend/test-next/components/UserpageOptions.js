3b2d3e172132139a23e69b0089e4e71a7ab149ab
ssarai1 <sunnyssarai98@gmail.com>
2019-04-01 21:46:24 -0500

    Optimize Userpage Script

--- Frontend/test-next/components/UserpageOptions.js
+++ Frontend/test-next/components/UserpageOptions.js
@@ -1,27 +1,32 @@
 class UserpageOptions extends React.Component {
 	
 	state = {
-		response: "Awaiting Response...",
-		responseAffirmative: 'S',
-		responseNegative: 'R'
+		response: 'Awaiting Response...',
+		responseAffirmative: '',
+		responseNegative: ''
 	}
+
 	
 
 	render() {
+		if (this.state.response === 'Continue to the'){
+			alert(document.getElementById('janedoe').selectedIndex)
+		}
 		return(
 			<div>
 				<div>
-					<h4>Please respond to the following questions to take you to the appropriate page.</h4>
+					<h4>Please respond to the question to take you to the appropriate page.</h4>
 					<p>Are you currently a member of bookmill?</p>
 				</div>
 				<form>
-					<select onChange = {e => this.setState({response : e.target.value})}>
-						<option value = "Continue to the">Yes</option>
-						<option value = "Continue to the">No</option>
+					<select id = 'janedoe' onChange = {e => this.setState({response : 'Continue to the'})}>
+						<option selected hidden disabled> -- Select one of the following. --</option>
+						<option id = 'danejoe' value = "jeff">Yes</option>
+						<option value = "fred">No</option>
 					</select>
 				</form>
 				<div>
-					<p id="foo">{this.state.response} <a id="yesLink" href="/signin">{this.state.responseAffirmative}</a> <a id="noLink" href="/MeetingSchedule">{this.state.responseNegative}</a></p>
+					<p>{this.state.response} <a id="yesLink" href="/signin">{this.state.responseAffirmative}</a> <a id="noLink" href="/MeetingSchedule">{this.state.responseNegative}</a></p>
 				</div>
 				<br/>
 				<br/>
