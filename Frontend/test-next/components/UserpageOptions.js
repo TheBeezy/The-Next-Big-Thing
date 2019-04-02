776baef550d9e86a8827ae6ea8145b7135f9c7a0
ssarai1 <sunnyssarai98@gmail.com>
2019-04-02 02:43:54 -0500

    Add script to create links on the fly

--- Frontend/test-next/components/UserpageOptions.js
+++ Frontend/test-next/components/UserpageOptions.js
@@ -2,14 +2,23 @@ class UserpageOptions extends React.Component {
 	
 	state = {
 		standardMsg: 'Awaiting Response...',
-		response : 'oof'
+		response : 'oof',
+		affirmativeResponse: '',
+		negativeResponse: ''
 	}
 	
 	
 	
 	render() {
-		if (this.state.standardMsg === 'Continue to the'){
-			alert(this.state.response);
+		if (this.state.response == 1){
+			this.state.standardMsg = 'Continue to the ',
+			this.state.affirmativeResponse = 'Sign In page.',
+			this.state.negativeResponse = ''
+		}
+		if (this.state.response == 2){
+			this.state.standardMsg = "You're new here! Continue to the next page to ",
+			this.state.negativeResponse = 'register!',
+			this.state.affirmativeResponse = ''
 		}
 		return(
 			<div>
@@ -18,14 +27,14 @@ class UserpageOptions extends React.Component {
 					<p>Are you currently a member of bookmill?</p>
 				</div>
 				<form>
-					<select onChange = {e => this.setState({standardMsg : 'Continue to the'})}>
+					<select onChange = {e => this.setState({response : e.target.selectedIndex})}>
 						<option selected hidden disabled> -- Select one of the following. --</option>
 						<option value = 'Sign-In Page'>Yes</option>
 						<option value = 'Registration Page'>No</option>
 					</select>
 				</form>
 				<div>
-					<p>{this.state.standardMsg} <a id="yesLink" href="/signin">{this.state.response}</a> <a id="noLink" href="/MeetingSchedule">{this.state.response}</a></p>
+					<p>{this.state.standardMsg} <a id="yesLink" href="/signin">{this.state.affirmativeResponse}</a> <a id="noLink" href="/MeetingSchedule">{this.state.negativeResponse}</a></p>
 				</div>
 				<br/>
 				<br/>
