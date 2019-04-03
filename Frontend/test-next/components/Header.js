2adb4415b01ab485b99c62f90c89ce778d4a9b00
Stephen Watson <sawblade25@gmail.com>
2019-04-02 21:03:23 -0500

    Update Header.js

--- Frontend/test-next/components/Header.js
+++ Frontend/test-next/components/Header.js
@@ -41,11 +41,6 @@ const Header = () => (
                 Sign In
             </a>
         </Link>
-        <Link href="/searchbook">
-            <a style={linkStyle}>
-                Book Search
-            </a>
-	    </Link>
     </div>
 )
 
