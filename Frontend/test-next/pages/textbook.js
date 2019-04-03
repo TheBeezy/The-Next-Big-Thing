70ded9033ae5509770e5f045a69e3ba5c4066777
Stephen Watson <sawblade25@gmail.com>
2019-04-02 22:45:34 -0500

    Update textbook.js

--- Frontend/test-next/pages/textbook.js
+++ Frontend/test-next/pages/textbook.js
@@ -2,6 +2,7 @@ import Layout from '../components/Layout'
 import firebase from 'firebase'
 import {withRouter} from 'next/router'
 import data from '../FireBaseConfig.json'
+import TextbookRater from '../components/TextbookRater'
 
 if (!firebase.apps.length) {
     firebase.initializeApp(data);
@@ -11,6 +12,9 @@ const textbook = withRouter(props => (
     <Layout>
         <h1>{props.router.query.id}</h1>
 		<p>Selling this book?<a href="javascript:window.open('/sellerPortal','mypopuptitle','width=600,height=400')">Add a Listing!</a></p>
+		<br/>
+		<p>Used this book? Leave a rating</p>
+		<TextbookRater/>
 		<br/>
 		<p>Looking for this book?  See who's selling below!</p>
     </Layout>
