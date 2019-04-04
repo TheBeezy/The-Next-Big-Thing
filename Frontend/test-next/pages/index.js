239188947d9c477fedba73eddacd40b3bce31f9e
Brandon Yao <TheBeezy@users.noreply.github.com>
2019-04-04 12:28:41 -0500

    Format index page

--- Frontend/test-next/pages/index.js
+++ Frontend/test-next/pages/index.js
@@ -6,9 +6,7 @@ import Fetch from 'isomorphic-unfetch'
 export default () => (
     
     <Layout>
-        <head>
-            <title>Home</title>
-        </head>
+        <title>Home</title>
         <p>
             <Search />
         </p>
