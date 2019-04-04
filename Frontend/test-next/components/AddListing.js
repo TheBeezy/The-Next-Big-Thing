9a7a709cc7239384a30bfc9d05378e195ff49626
Stephen Watson <sawblade25@gmail.com>
2019-04-04 14:07:05 -0500

    Update AddListing.js
    still needs work, but helped from Grant has us moving in the right direction here.

--- Frontend/test-next/components/AddListing.js
+++ Frontend/test-next/components/AddListing.js
@@ -19,7 +19,7 @@ class AddListing extends React.Component {
   constructor(props) {
     super(props);
     // Holds the values for the text fields
-    this.state = { listings: [], description: ''};
+    this.state = {description: ''};
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -40,10 +40,25 @@ class AddListing extends React.Component {
     // Stops the page from refreshing
     event.preventDefault();
     // Sets the data based on text fields
-    var setTextBook = db.collection('textbooks').doc(this.state.description).set(prevState => ({
+    /*var setTextBook = db.collection('textbooks').doc(this.state.description).set(prevState => ({
              listings: [...prevState.listings,this.state.description],
-        }))
-    
+        }))*/
+	db.runTransaction(function(tx) {
+		
+		tx.get(db.collection('listings')).add({
+			book: this.props.listing,
+			date: new Date().toISOString(),
+			description: document.getElementById('description-box').value,
+			user: 1234
+		}).then(function(listing) {
+			
+			tx.get(db.collection('textbooks').doc("the textbook id").doc(listing)).add({
+				//add the listing to the set of listings
+				
+			})
+	
+		}
+    }
   }
 
   // Form for submitting information (note the names)
@@ -55,6 +70,7 @@ class AddListing extends React.Component {
             Desription:
               <textarea
               name="description"
+			  id="description-box"
               type="text"
 			  width="90%"
 			  height="80%"
