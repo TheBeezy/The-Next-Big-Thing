dc5b57d844ee822f5e9c6d0253160a62451e1ef5
Austin Lee <alee62@lsu.edu>
2019-04-07 20:05:42 -0500

    Formatting
    Formatting

--- Frontend/test-next/pages/textbook.js
+++ Frontend/test-next/pages/textbook.js
@@ -7,11 +7,12 @@ import Seller from '../components/Seller'
 const textbook = withRouter(props => (
     <Layout> 
         <Textbook id={props.router.query.id}/>
+		<p>Looking for this book?  See who's selling below!</p>
+		<Seller id={props.router.query.id}/>
+		<br/>
 		<p>Used this book? Leave a rating</p>
 		<TextbookRater/>
 		<br/>
-		<p>Looking for this book?  See who's selling below!</p>
-		<Seller id={props.router.query.id}/>
     </Layout>
 ))
 export default textbook
